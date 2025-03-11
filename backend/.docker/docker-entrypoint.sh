e9881789c0b9d2e3bb9be937bcb0144f8f9dfad9
GabrielLima2803 <gabriellima2803@gmail.com>
2025-03-11 18:15:05 -0300

    fix: Corrigindo Vendor

--- backend/.docker/docker-entrypoint.sh
+++ backend/.docker/docker-entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -e
 
+if [ ! -d "vendor" ]; then
+  composer install --optimize-autoloader --no-dev --no-interaction
+fi
+
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+  php artisan key:generate
+fi
+
 while ! mysqladmin ping -h"db" -u"root" -p"secret" --silent; do
     sleep 2
 done
